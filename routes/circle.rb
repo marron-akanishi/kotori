4396c1d5210086e49ab636cbda1c8487b10c767a
Marron Akanishi <marron.general@gmail.com>
2020-03-16 16:34:35 +0900

    読み修正機能の開放

--- routes/circle.rb
+++ routes/circle.rb
@@ -14,6 +14,20 @@ class App < Sinatra::Base
     else
       @is_adult = User.find(session[:id]).is_adult
     end
+    message = {
+      "mod_yomi" => "読みを修正しました"
+    }
+    @msg = message[params["msg"]]
     erb :detail
+  end
+
+  post '/circle/:id/mod_yomi' do
+    login_check
+    begin
+      Circle.find(params["id"]).update(name_yomi: normalize_str(params["yomi"]))
+    rescue => e
+      redirect to("/error?code=422")
+    end
+    redirect to("/circle/#{params["id"]}?msg=mod_yomi")
   end
 end
